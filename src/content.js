1e91f35369349a40843ce4ac9cc9dd3af5835047
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2024-04-21 13:19:42 +0200

    Ver. 2.0: optimalization, better functionality new design.

--- src/content.js
+++ src/content.js
@@ -1,37 +1,35 @@
-function addCustomButton() {
-    var actionsDiv = document.querySelector(".navigation-container");
+document.addEventListener("DOMContentLoaded", () => {
+    const actionsDiv = document.querySelector(".navigation-container");
 
     if (actionsDiv) {
-        clearInterval(checkExistenceInterval);
-        
+        // log success
         console.log("YouTube Shorts Control successfully loaded!");
 
         // to fit custom button with other buttons
         actionsDiv.style.justifyContent = "flex-end";
-        let buttons = actionsDiv.querySelectorAll(".navigation-button.ytd-shorts");
+        const buttons = actionsDiv.querySelectorAll(".navigation-button.ytd-shorts");
         buttons.forEach(button => {
             button.style.padding = "8px 16px";
         });
 
         // create custom button
-        var newButton = document.createElement("button");
+        const newButton = document.createElement("button");
         newButton.className = "yt-shorts-control";
         newButton.innerHTML = "Open as video";
 
         actionsDiv.appendChild(newButton);
 
-        // add event listener
-        newButton.addEventListener("click", function () {
-            var currentUrl = window.location.href;
-
+        // add event listener with arrow function
+        newButton.addEventListener("click", () => {
+            const currentUrl = window.location.href;
             if (currentUrl.includes("shorts/")) {
-                var newUrl = currentUrl.replace("shorts/", "watch?v=");
-
-                window.location.href = newUrl;
+                // create a new link
+                const newUrl = currentUrl.replace("shorts/", "watch?v=");
+                // pause all videos
+                document.querySelectorAll('video').forEach(v => v.pause());
+                // open new url
+                window.open(newUrl, "_blank");
             }
         });
     }
-}
-
-// check if the navigation container already exists
-var checkExistenceInterval = setInterval(addCustomButton, 5000);
+})
--- src/popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var closeButton = document.getElementById("closeButton");
-
-    closeButton.addEventListener("click", function () {
-        window.close();
-    });
-});
